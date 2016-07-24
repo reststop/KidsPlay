d20ca372a4680152963ebfc0766e2cbd0be41006
AMIT NARAYAN <amit.narayan@gmail.com>
2016-07-23 21:56:05 -0700

    merge swiper view

---
+++ KidsPlay/KidsPlay-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "DraggableView.h"
+#import "DraggableViewBackground.h"
+#import "OverlayView.h"
